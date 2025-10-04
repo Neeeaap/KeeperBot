4af346d3fd63a96c75222847c2b8c9d8be8b0e8a
Neeeaap <joenepnep@gmail.com>
2025-10-05 02:06:35 +0800

    First step commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-let y = 1;
+require("dotenv").config();
+
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const eventLogChannelId = "1424080551609892934";
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Client online, logged in as ${readyClient.user.tag}`)
+});
+
+client.on(Events.MessageCreate, message => {
+    if (message.author.bot) return;
+    if (message.channel.id !== eventLogChannelId) return;
+
+    console.log(message.content);
+})
+
+client.login(process.env.BOT_TOKEN);
